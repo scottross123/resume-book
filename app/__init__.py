1f4f29b3b3db452c5feae3e7e15e63c7a5d49e34
Scott Ross <scott.william.ross2@gmail.com>
2022-10-25 11:14:03 -0400

    basic file uploads working and restricted to pdf

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,17 @@
-import os
-
 from flask import Flask
 
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='secret')
+    app.config['UPLOAD_FOLDER'] = './uploads'
+    # max file size 5mb
+    app.config['MAX_CONTENT_LENGTH'] = 5 * 1000 * 1000
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
-
 
     from . import form
     app.register_blueprint(form.bp)
--- app/form.py
+++ app/form.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, flash, current_app
+import os
+from werkzeug.utils import secure_filename
 from app.googleapi import DriveService, get_creds
 import gspread
 from gspread import Cell
@@ -15,6 +17,15 @@ gc = gspread.service_account(filename=SECRETS)
 internships = gc.open('Resumes').worksheet("Internship")
 fulltime = gc.open('Resumes').worksheet("Full-Time")
 
+ALLOWED_EXTENSIONS = {'pdf'}
+
+
+def allowed_file(filename: str):
+    print('filename? ', filename)
+    print('period? ', '.' in filename)
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def add_entry(entry: dict, worksheet):
     email_cell = worksheet.find(entry['email'])
@@ -27,20 +38,27 @@ def add_entry(entry: dict, worksheet):
         # Cell(row, col, value), creates new cell objects with updated values
         updated_cells.append(Cell(email_cell.row, i+1, entry_values[i]))
     print("updated cells: ", updated_cells)
+    flash(f'Email {entry["email"]} was found on record, replaced with new info')
     return worksheet.update_cells(updated_cells)
 
 
 @bp.route("/", methods=['GET', 'POST'])
 def index():
-    #drive.list_files(10, True)
     if request.method == 'POST':
         entry = {
             "f_name": request.form['f_name'],
             "l_name": request.form['l_name'],
             "email": request.form['email'],
             "last_modified": datetime.now().ctime(),
-            "resume": request.form['resume']
+            "resume": request.files['resume'].filename
         }
+        resume = request.files['resume']
+        print('resume exists', resume)
+        print('allowed', allowed_file(resume.filename))
+        if resume and allowed_file(resume.filename):
+
+            filename = secure_filename(resume.filename)
+            resume.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
         if request.form['job'] == 'internship':
            add_entry(entry, internships)
         if request.form['job'] == 'full-time':
