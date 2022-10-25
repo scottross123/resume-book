207131f38c7bbb7ff3742693a66b6d88f46151a4
Scott Ross <scott.william.ross2@gmail.com>
2022-10-25 10:16:39 -0400

    row updates with new info when email exists in sheet

--- app/form.py
+++ app/form.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect
 from app.googleapi import DriveService, get_creds
 import gspread
+from gspread import Cell
 from datetime import datetime
 
 bp = Blueprint('form', __name__, static_folder="static", template_folder='templates')
@@ -15,18 +16,36 @@ internships = gc.open('Resumes').worksheet("Internship")
 fulltime = gc.open('Resumes').worksheet("Full-Time")
 
 
+def add_entry(entry: dict, worksheet):
+    email_cell = worksheet.find(entry['email'])
+    entry_values = [entry[key] for key in entry]
+    print("entry values: ", entry_values)
+    if email_cell is None:
+        return worksheet.append_row(entry_values)
+    updated_cells = []
+    for i in range(0, len(entry_values)):
+        # Cell(row, col, value), creates new cell objects with updated values
+        updated_cells.append(Cell(email_cell.row, i+1, entry_values[i]))
+    print("updated cells: ", updated_cells)
+    return worksheet.update_cells(updated_cells)
+
+
 @bp.route("/", methods=['GET', 'POST'])
 def index():
     #drive.list_files(10, True)
     if request.method == 'POST':
-        form = request.form
-        data = [form['f_name'], form['l_name'], form['email'], datetime.now().isoformat(), form['resume']]
-        print(form['job'])
-        if form['job'] == 'internship':
-            internships.append_row(data)
-        if form['job'] == 'full-time':
-            fulltime.append_row(data)
-        print(data)
+        entry = {
+            "f_name": request.form['f_name'],
+            "l_name": request.form['l_name'],
+            "email": request.form['email'],
+            "last_modified": datetime.now().isoformat(),
+            "resume": request.form['resume']
+        }
+        if request.form['job'] == 'internship':
+           add_entry(entry, internships)
+        if request.form['job'] == 'full-time':
+           add_entry(entry, fulltime)
+        print(entry)
 
         return redirect('/')
     return render_template('index.html')
