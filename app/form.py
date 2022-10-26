7cf2e4ea23464001b5e0b3d1798a139ad772cc19
Scott Ross <scott.william.ross2@gmail.com>
2022-10-25 20:46:51 -0400

    added file upload to google drive in memory

--- app/form.py
+++ app/form.py
@@ -1,6 +1,11 @@
+import io
+
 from flask import Blueprint, render_template, request, redirect, flash, current_app
 import os
 from werkzeug.utils import secure_filename
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+from oauth2client.service_account import ServiceAccountCredentials
 from app.googleapi import DriveService, get_creds
 import gspread
 from gspread import Cell
@@ -9,20 +14,24 @@ from datetime import datetime
 bp = Blueprint('form', __name__, static_folder="static", template_folder='templates')
 SECRETS = './secrets.json'
 SCOPES = ['https://www.googleapis.com/auth/drive']
-creds = get_creds(SECRETS, SCOPES)
-drive = DriveService(creds)
-print("drive creds: ", drive.creds)
 
 gc = gspread.service_account(filename=SECRETS)
 internships = gc.open('Resumes').worksheet("Internship")
 fulltime = gc.open('Resumes').worksheet("Full-Time")
 
+gauth = GoogleAuth()
+gauth.auth_method = 'service'
+gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name(SECRETS, SCOPES)
+drive = GoogleDrive(gauth)
+
 ALLOWED_EXTENSIONS = {'pdf'}
+
+PARENT_FOLDER_ID = '1hwLdGhQ8gt36TrLMMbkZ0kUXX_3Qkfd5'
+INTERNSHIP_FOLDER_ID = '1cKXq0Tya5iP2UHG2WbUN8eivq0g8IwCn'
+FULLTIME_FOLDER_ID = '1D4HTAiu1Csxn952q-U8_-VCp6o5K_u-F'
 
 
 def allowed_file(filename: str):
-    print('filename? ', filename)
-    print('period? ', '.' in filename)
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
@@ -38,8 +47,23 @@ def add_entry(entry: dict, worksheet):
         # Cell(row, col, value), creates new cell objects with updated values
         updated_cells.append(Cell(email_cell.row, i+1, entry_values[i]))
     print("updated cells: ", updated_cells)
-    flash(f'Email {entry["email"]} was found on record, replaced with new info')
+    # flash(f'Email {entry["email"]} was found on record, replaced with new info')
     return worksheet.update_cells(updated_cells)
+
+
+def create_file(file, parent_id: str, mime_type: str):
+    try:
+        new_file = drive.CreateFile({
+            'title': file.filename,
+            'mimeType': mime_type,
+            'parents': [{'id': parent_id}]
+        })
+        file.seek(0)
+        buffer = io.BytesIO(file.read())
+        new_file.content = buffer
+        return new_file.Upload()
+    except Exception as e:
+        return print(e)
 
 
 @bp.route("/", methods=['GET', 'POST'])
@@ -53,24 +77,15 @@ def index():
             "resume": request.files['resume'].filename
         }
         resume = request.files['resume']
-        print('resume exists', resume)
-        print('allowed', allowed_file(resume.filename))
         if resume and allowed_file(resume.filename):
-
             filename = secure_filename(resume.filename)
             resume.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
         if request.form['job'] == 'internship':
-           add_entry(entry, internships)
+            add_entry(entry, internships)
+            create_file(resume, INTERNSHIP_FOLDER_ID, 'application/pdf')
         if request.form['job'] == 'full-time':
-           add_entry(entry, fulltime)
+            add_entry(entry, fulltime)
+            create_file(resume, FULLTIME_FOLDER_ID, 'application/pdf')
         print(entry)
-
         return redirect('/')
     return render_template('index.html')
-
-
-@bp.route("/list", methods=['GET'])
-def list():
-    files = drive.list_files(10)
-    return files
-
